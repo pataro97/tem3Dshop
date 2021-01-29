5d11432a527b27ff5430cd53c6c6e64c2fde9c62
pataro97 <pablo.tamayo.romero@gmail.com>
2021-01-29 21:11:51 +0100

    1.26.21

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,8 @@ export class HomeComponent implements OnInit {
 
 
 
-  @ViewChild('rendererCanvas', {static: true})
-  public rendererCanvas: ElementRef<HTMLCanvasElement>;
+  @ViewChild('rendererCanvas1', {static: true}) //true or false si no es estatico y depende de una variable(*ngIf)
+  public rendererCanvas1: ElementRef<HTMLCanvasElement>;
 
 
   public constructor(private viewer: Viewer3dService) {
@@ -24,10 +24,17 @@ export class HomeComponent implements OnInit {
     
   }
 
-  public loadCanvas(): void {
+  public loadCanvas(id) {
+    var target = id.target || id.srcElement || id.currentTarget;
+    var idAttr = target.attributes.id; //selecciona atributo
+    var value = idAttr.nodeValue; //seleciona lo que hay dentro del atributo id
+    
+    alert(value)
+    // rend canvas
+
     $('#canvas').removeAttr('hidden');
     $('#carousel').hide().css("display", "none");
-    this.viewer.createScene(this.rendererCanvas, 'https://cdn.coursesaver.com/files/Part26-4.STL', this.showCanvas);
+    this.viewer.createScene(this.rendererCanvas1, 'https://cdn.coursesaver.com/files/Part26-4.STL', this.showCanvas);
     this.viewer.animate();
   }
 
